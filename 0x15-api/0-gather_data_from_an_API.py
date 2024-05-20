8e2abf7c7898f59a4a6a243ee51b59e52375983b
root <root@DL>
2024-05-21 01:26:36 +0300

    feat: Add script to fetch and display employee TODO list progress
    - Implemented script to fetch employee data and TODO list from a REST API
    - Added error handling for invalid employee ID and failed API requests
    - Filtered and displayed completed tasks with proper formatting

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,49 +5,22 @@ import requests
 import sys
 
 
-def fetch_employee_data(employee_id):
-    # Base URL of the API
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{base_url}/users/{employee_id}"
-    employee_response = requests.get(employee_url)
-    if employee_response.status_code != 200:
-        print("Error fetching employee data")
-        return
-
-    # Fetch todos data
-    employee_data = employee_response.json()
-    EMPLOYEE_NAME = employee_data.get('name')
-
-    # Fetch employees TODO list
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-    todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
-        print("Error fetching TODO list")
-        return
-
-    todos_data = todos_response.json()
-
-    # Flter completed tasks
-    completed_tasks = [task for task in todos_data if task.get('completed')]
-    TOTAL_NUMBER_OF_TASKS = len(todos_data)
-    NUMBER_OF_DONE_TASKS = len(completed_tasks)
-
-    # print the required tasks
-    print(f"Employee {EMPLOYEE_NAME} is done with tasks "
-          f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-    for task in completed_tasks:
-        print(f"\t {task.get('title')}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py <employee_id>")
-        sys.exit(1)
+    # Define the URL for the REST API
+    url = "https://jsonplaceholder.typicode.com/"
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an Integer")
-        sys.exit(1)
+    # send a GET request to retrieve user info
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
 
-    fetch_employee_data(employee_id)
+    # send a GET request to retrive the TODO list
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    # filter completed TODO list and store titles in a list
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+
+    # print employee's name, completed tasks & total no of tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    # print the titles of completed tasks with indentation
+    [print("\t {}".format(c)) for c in completed]
